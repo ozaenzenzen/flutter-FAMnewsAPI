bd3e23dfff8d99fb92d912bca2fb1ed533b26f5b
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-07-11 08:59:38 +0700

    cleaning code

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_newsapi1/connect_api.dart';
-import 'package:flutter_newsapi1/articles_api.dart';
-import 'package:flutter_newsapi1/newsUI.dart';
+import 'package:flutter_newsapi1/screens/home.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "FAM Flutter API  - Berita Indonesia",
-    home: new HomePage(),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _HomePageState extends State<HomePage> {
-  ConnectAPI connectAPI = ConnectAPI();
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.white,
-        elevation: 1,
-        centerTitle: true,
-        title: new Text(
-          "FAM Flutter API - Berita Indonesia",
-          style: new TextStyle(color: Colors.black),
-        ),
-      ),
-      body: new FutureBuilder(
-        future: connectAPI.getArtcile(),
-        builder:
-            (BuildContext context, AsyncSnapshot<List<ArticlesAPI>> snapshot) {
-          if (snapshot.hasData) {
-            List<ArticlesAPI> articles = snapshot.data;
-            return ListView.builder(
-              itemCount: articles.length,
-              itemBuilder: (context, index) {
-                return newsUI(articles[index], context);
-              },
-            );
-          }
-          return new Center(
-            child: new CircularProgressIndicator(),
-          );
-        },
-      ),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "FAM Flutter API  - Berita Indonesia",
+      home: new HomePage(),
     );
   }
 }
